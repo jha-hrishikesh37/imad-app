827dfb4f6736ed2efc6ec53705165cd13ddb0fd3
jha-hrishikesh37 <36738460+jha-hrishikesh37@users.noreply.github.com>
2018-02-28 21:39:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articlename', function (req, res) {
-  var articlename = req.param.articlename;
+  var articlename = req.params.articlename;
   res.send(createTemplate(articles[articlename]));
 });
 
