1d40b906630dd6197d5e437e5a789c291370c688
jha-hrishikesh37 <36738460+jha-hrishikesh37@users.noreply.github.com>
2018-03-02 15:25:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,15 @@
 console.log('Loaded!');
 
+//Change the text of the main body
+
 var element = document.getElementById('maintext');
 
 element.innerHTML = 'New Code';
+
+//Move the image
+
+var img = document.getElementById('madi');
+
+img.onclink = function(){
+  img.style.marginleft = '100px';  
+};
