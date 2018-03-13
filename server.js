700c1b65047ffa1a8ed9e576c691ba2431af0d48
jha-hrishikesh37 <36738460+jha-hrishikesh37@users.noreply.github.com>
2018-03-13 23:09:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,6 +103,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
