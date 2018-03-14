a02b956341a03b0e998a2e20c2fd5634aa7b98c1
jha-hrishikesh37 <36738460+jha-hrishikesh37@users.noreply.github.com>
2018-03-15 00:36:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -49,8 +49,6 @@ button.onclick = function() {
 };
 
 //Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
   //Make a request to the server and save the same
@@ -79,6 +77,8 @@ submit.onclick = function() {
     };
     
    //Make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET','http://jhahrishikesh37.imad.hasura-app.io/submit-name/?name=' + name,true);
     request.send(null);
   
